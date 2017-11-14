6e7e41f49411c4778334cb6200f5c496c1b3d6d4
Rob | Labs <roblabs@users.noreply.github.com>
2017-11-13 21:08:29 -0800

    Extra checks for existing files

--- usgs.sh
+++ usgs.sh
@@ -39,9 +39,15 @@ getJsonPDF() {
   export name=$(cat /tmp/name.txt)
   echo $name-$base
 
-  wget -N $pdfUrl -P tmp
-  cp tmp/$base.pdf pdf/$name-$base.pdf
-  ls -l pdf/$name-$base.pdf
+  if [ -f pdf/$base.pdf ]
+  then
+      echo pdf/$base.pdf exists
+  else
+      echo pdf/$base.pdf does not exist
+      wget -N $pdfUrl -P pdf
+      cp pdf/$base.pdf pdf/$name-$base.pdf
+      ls -l pdf/$name-$base.pdf
+  fi
 
   # 4. get bounding box
   # After knowing the dimensions of the GeoTIFF, then extract just the map (no collar)
@@ -67,59 +73,88 @@ getJsonPDF() {
 # Crop out just the map data
 #  -te xmin ymin xmax ymax:
 makeTopo() {
-  gdal_translate pdf/$name-$base.pdf tmp/$name-$base.topo.$DPI.tif \
-    -co COMPRESS=LZW \
-    --config GDAL_PDF_LAYERS "all" \
-    --config GDAL_PDF_DPI $DPI \
-    --config GDAL_PDF_LAYERS_OFF "Images"
 
-  gdalwarp -t_srs EPSG:4326 -dstalpha \
-    -co COMPRESS=LZW \
-    tmp/$name-$base.topo.$DPI.tif tmp/$name-$base.topo.crop.$DPI.tif \
-    -te $xmin $ymin $xmax $ymax
+  if [ -f $FRIENDLY_TMP_FOLDER/$name-$base.topo.$DPI.tif ]
+  then
+      echo $FRIENDLY_TMP_FOLDER/$name-$base.topo.$DPI.tif exists
+  else
+    gdal_translate pdf/$name-$base.pdf $FRIENDLY_TMP_FOLDER/$name-$base.topo.$DPI.tif \
+      -co COMPRESS=LZW \
+      --config GDAL_PDF_LAYERS "all" \
+      --config GDAL_PDF_DPI $DPI \
+      --config GDAL_PDF_LAYERS_OFF "Images"
+
+    gdalwarp -t_srs EPSG:4326 -dstalpha \
+      -co COMPRESS=LZW \
+      $FRIENDLY_TMP_FOLDER/$name-$base.topo.$DPI.tif $FRIENDLY_TMP_FOLDER/$name-$base.topo.crop.$DPI.tif \
+      -te $xmin $ymin $xmax $ymax
+  fi
+
 }
 
+makeFSTopo() {
+  if [ -f $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.tif ]
+  then
+    echo FSTopo exists = $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.tif
+    echo " "
+  else
+    echo FSTopo, creating $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.tif
+
+    gdal_translate pdf/$FSTOPO $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.tif \
+      -co COMPRESS=LZW \
+      --config GDAL_PDF_LAYERS_OFF "Quadrangle.Contour_Labels,Quadrangle.Contours,Quadrangle.UTM_Grid" \
+      --config GDAL_PDF_DPI $FSTOPO_DPI
+
+    gdalwarp -t_srs EPSG:4326 -dstalpha \
+      -co COMPRESS=LZW \
+      $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.tif $FRIENDLY_TMP_FOLDER/$FSTOPO.$FSTOPO_DPI.crop.tif \
+      -te $xmin $ymin $xmax $ymax
+  fi
+}
 
 ### USGS TOPO Raster images
 makeOrthoImage() {
-  gdal_translate pdf/$name-$base.pdf tmp/$name-$base.ortho.$DPI.tif \
+  gdal_translate pdf/$name-$base.pdf $FRIENDLY_TMP_FOLDER/$name-$base.ortho.$DPI.tif \
     -co COMPRESS=LZW \
     --config GDAL_PDF_LAYERS "Images.Orthoimage" \
     --config GDAL_PDF_DPI $DPI
 
   gdalwarp -t_srs EPSG:4326 -dstalpha \
     -co COMPRESS=LZW \
-    tmp/$name-$base.ortho.$DPI.tif tmp/$name-$base.ortho.crop.$DPI.tif \
+    $FRIENDLY_TMP_FOLDER/$name-$base.ortho.$DPI.tif $FRIENDLY_TMP_FOLDER/$name-$base.ortho.crop.$DPI.tif \
     -te $xmin $ymin $xmax $ymax
 }
 
 
 makeShaded_Relief() {
-  if [ -f tmp/$name-$base.relief.$DPI.tif ]
+  echo
+  echo "##########"
+  echo $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif
+  if [ -f $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif ]
   then
-      echo the file exists
+      echo   SKIPPING, exists = $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif
   else
-      echo the file does not exist
+      echo   SHADED RELIEF, creating $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif
 
-      gdal_translate pdf/$name-$base.pdf tmp/$name-$base.relief.$DPI.tif \
+      gdal_translate pdf/$name-$base.pdf $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif \
         -co COMPRESS=LZW \
         --config GDAL_PDF_LAYERS "Map_Collar.Map_Elements,Map_Frame.Terrain.Shaded_Relief" \
         --config GDAL_PDF_DPI $DPI \
 
       gdalwarp -t_srs EPSG:4326 -dstalpha \
       -co COMPRESS=LZW \
-      tmp/$name-$base.relief.$DPI.tif tmp/$name-$base.relief.crop.$DPI.tif \
+      $FRIENDLY_TMP_FOLDER/$name-$base.relief.$DPI.tif $FRIENDLY_TMP_FOLDER/$name-$base.relief.crop.$DPI.tif \
         -te $xmin $ymin $xmax $ymax
   fi
 }
 
 makeWebP() {
   Run GDAL
-  ./go-single.sh  tmp/$name-$base.relief.crop.$DPI.tif $name-$base.$DPI   ###########
+  ./go-single.sh  $FRIENDLY_TMP_FOLDER/$name-$base.relief.crop.$DPI.tif $name-$base.$DPI   ###########
 }
 
 
-while getopts "TRWD:S:O" opt; do
+while getopts "D:F:N:P:S:TRWO" opt; do
   case $opt in
     T)
       echo "-T to produce a topo" >&2
@@ -137,6 +172,20 @@ while getopts "TRWD:S:O" opt; do
       export DPI="$OPTARG"
       echo DPI = $DPI >&2
       ;;
+    F)
+      export FSTOPO="$OPTARG"
+      echo FSTOPO = $FSTOPO >&2
+      makeFSTopo
+      ;;
+    P)
+      export FSTOPO_DPI="$OPTARG"
+      echo FSTOPO_DPI = $FSTOPO_DPI >&2
+      ;;
+    N)
+      export FRIENDLY_TMP_FOLDER="tmp/$OPTARG"
+      echo FRIENDLY_TMP_FOLDER = $FRIENDLY_TMP_FOLDER >&2
+      mkdir -p $FRIENDLY_TMP_FOLDER
+      ;;
     S)
       export sourceId="$OPTARG"
       echo sourceId = $sourceId >&2
@@ -148,7 +197,7 @@ while getopts "TRWD:S:O" opt; do
     \?)
       echo "Invalid option: -$OPTARG" >&2
       echo "Usage" >&2
-      echo "  sh usgs.sh -S 5825aec2e4b01fad86dd149c -D 512 -T -O -R" >&2
+      echo "  sh usgs.sh -S 5825aec2e4b01fad86dd149c -D 300 -T -O -R" >&2
       ;;
   esac
 done
